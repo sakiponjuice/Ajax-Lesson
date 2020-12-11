c507729b90a0b6a825cee226707d6b306b6253cd
sakiponjuice <saki.pon.juice0922@gmail.com>
2020-12-11 13:58:56 +0900

    [Add] Ajaxを学ぼう

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
-$('#tab-contents .tab[id != "tab1"]').hide();
-
-$('#tab-menu a').on('click', function (event) {
-  $("#tab-contents .tab").hide();
-  $("#tab-menu .active").removeClass("active");
-  $(this).addClass("active");
-  $($(this).attr("href")).show();
-  event.preventDefault();
+API_KEY = "c642ac0c4525d306e7ba476b5a5e7637";
+$(function () {
+  $('#btn').on('click', function () {
+    $.ajax({
+      url: "http://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType: 'jsonp',
+    }).done(function (data) {
+      $('#place').text(data.name);
+      $('#temp_max').text(data.main.temp_max);
+      $('#temp_min').text(data.main.temp_min);
+      $('#humidity').text(data.main.humidity);
+      $('#speed').text(data.wind.speed);
+      $('#weather').text(data.weather[0].main);
+      $('img').attr("src", "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png");
+      $('img').attr("alt", data.weather[0].main);
+    }).fail(function (data) {
+      alert('通信に失敗しました。');
+    });
+  });
 });
